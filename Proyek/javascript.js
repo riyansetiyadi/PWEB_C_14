96ec5faa586948bf3860295b527da9d8f4ea9beb
Riyansetiyadi <setiyariyan19@gmail.com>
2020-11-07 23:29:17 +0700

    memperbaiki javascript notif

--- Proyek/javascript.js
+++ Proyek/javascript.js
@@ -1,4 +1,4 @@
-function Masuk(){
+function masuk(){
     var email = document.forms["masuk"]["email"].value;
     var password = document.forms["masuk"]["password"].value;
     if (email == "") {
@@ -9,6 +9,18 @@ function Masuk(){
         return false;
     } 
     alert("Anda berhasil masuk")
+}
+function daftar(){
+    var email = document.forms["daftar"]["email"].value;
+    var password = document.forms["daftar"]["password"].value;
+    if (email == "") {
+        alert("Email harus diisi");
+        return false;
+    } else if (password == "") {
+        alert("Password harus diisi");
+        return false;
+    } 
+    alert("Anda berhasil mendaftar, silahkan masuk")
 }
 function ubahPassword(){
     var mail = document.forms["ubah"]["email"].value;
