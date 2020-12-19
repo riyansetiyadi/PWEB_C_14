bef0ff71cf0a266622729b3bbeb34a8a3b382bb7
arkartika <71867729+arkartika@users.noreply.github.com>
2020-12-19 23:22:43 +0700

    mengubah menjadi string

--- Proyek/javascript.js
+++ Proyek/javascript.js
@@ -34,47 +34,47 @@ function ubahPassword(){
 }
 
 let Jagung = {
-    nama : "jagung",
-    harga : 5000,
-    toko  :"Rosa olshop",
+    "nama" : "jagung",
+    "harga" : 5000,
+    "toko"  :"Rosa olshop",
 }
 let Beras = {
-    nama : "Beras",
-    harga : 10000,
-    toko : "Rosa olshop"
+    "nama" : "Beras",
+    "harga" : 10000,
+    "toko" : "Rosa olshop"
 }
 let Cabai = {
-    nama : "Cabai",
-    harga : 20000,
-    toko : "Rosa olshop"
+    "nama" : "Cabai",
+    "harga" : 20000,
+    "toko" : "Rosa olshop"
 }
 let Jeruk = {
-    nama : "jeruk",
-    harga : 10000,
-    toko : "Rosa olshop"
+    "nama" : "jeruk",
+    "harga" : 10000,
+    "toko" : "Rosa olshop"
 }
 let Kentang = {
-    nama : "Kentang",
-    harga : 15000,
-    toko : "Rosa olshop"
+    "nama" : "Kentang",
+    "harga" : 15000,
+    "toko" : "Rosa olshop"
 }
 let Semangka = {
-    nama : "Semangka",
-    harga : 8000,
-    toko : "Rosa olshop"
+    "nama" : "Semangka",
+    "harga" : 8000,
+    "toko" : "Rosa olshop"
 }
 let Kubis = {
-    nama : "Kubis",
-    harga : 2500,
-    toko : "Rosa olshop"
+    "nama" : "Kubis",
+    "harga" : 2500,
+    "toko" : "Rosa olshop"
 }
 let Terong = {
-    nama : "Terong",
-    harga : 5000,
-    toko : "Rosa olshop"
+    "nama" : "Terong",
+    "harga" : 5000,
+    "toko" : "Rosa olshop"
 }
 let Bombay = {
-    nama : "Bombay",
-    harga : 3000,
-    toko : "Rosa olshop"
+    "nama" : "Bombay",
+    "harga" : 3000,
+    "toko" : "Rosa olshop"
 }
