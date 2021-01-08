5e5347066d0cb35f3f9e1eb3496c2a4996e4a498
Riyansetiyadi <setiyariyan19@gmail.com>
2021-01-08 22:11:27 +0700

    perbaiki navigasi

--- Proyek/javascript.js
+++ Proyek/javascript.js
@@ -130,8 +130,8 @@ $(function() {
     })
 })
 
-$('.dropdown-child').prepend("<a href='index.html' class='navigasi' id='teks-lapaktani'><span>LAPAK</span>TANI</a>");
-$('.dropdown-child').append("<a href='masuk.html' class='navigasi'>Masuk</a><a href='daftar.html' class='navigasi'>Daftar</a>");
+$('.dropdown-child').prepend("<a href='index.html' class='navigasi hilang-dekstop' id='teks-lapaktani'><span>LAPAK</span>TANI</a>");
+$('.dropdown-child').append("<a href='masuk.html' class='navigasi hilang-dekstop'>Masuk</a><a href='daftar.html' class='navigasi'>Daftar</a>");
 $('.tombol-dropdown').on('click', function(){
     $(".dropdown-child").css("display", "flex");
 })
